f626824fda2d8485c8f1285778a9e3e53d17ac31
siri-2020 <tanyakarn.siri2020@gmail.com>
2026-02-05 19:32:24 +0700

    tested

---
+++ Gaussian elimination in naive form.py
@@ -0,0 +1,43 @@
+def gaussian_elimination(A, b):
+    A = [row[:] for row in A]
+    b = b[:]
+    n = len(A)
+    
+    # Forward pass of elimination
+    for i in range(n-1):
+        for j in range(i+1, n):
+            k_temp = - A[j][i] / A[i][i]
+            print(f"Step {i+1} {j+1}: k = {k_temp:.2f}")
+            temp_row = [a * k_temp for a in A[i]]
+            print(f"Temp row: {temp_row}")
+            for k in range(n):
+                A[j][k] += temp_row[k]
+            b[j] += k_temp * b[i]
+            print(f"Updated A = {A}")
+            print(f"Updated b = {b}")
+    
+    x_sol = [None for _ in range(n)]
+    for i in range(n):
+        row_idx = n - i - 1
+        res = b[row_idx]
+        for j in range(n - i, n):
+            res -= A[row_idx][j] * x_sol[j]
+        x_sol[row_idx] = res / A[row_idx][row_idx]
+        print(f"x_{n-i-1} = {x_sol[n-i-1]:.2f}")
+    
+    print(f"Solution: {x_sol}")
+    return x_sol
+
+
+#===================TEST CASES====================
+A1 = [[1, 1, 1], [3, 2, 1], [2, -1, 4]]
+b1 = [6, 10, 12]
+gaussian_elimination(A1, b1)
+print('--------------------------------')
+A2 = [[2, 1], [1, 3]]
+b2 = [5, 6]
+gaussian_elimination(A2, b2)
+print('--------------------------------')
+A3 = [[1, 2, 1], [2, 3, 4], [4, 3, 2]]
+b3 = [8, 20, 16]
+gaussian_elimination(A3, b3)
